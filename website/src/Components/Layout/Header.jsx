52eee38cdcd9bc5fc38f16c6d78f4db6e8a1f658
Adhiraj <adhirajghadge11@gmail.com>
2024-12-14 13:20:20 +0530

    Navbar Component

--- website/src/Components/Layout/Header.jsx
+++ website/src/Components/Layout/Header.jsx
@@ -1,7 +1,9 @@
+import Sidebar from "./Sidebar"
 const Header = () => {
     return (
         <>
-            <p className="bg-blue-500 text-white p-4">Header</p>
+            {/* <p className="bg-blue-500 text-white p-4">Header</p> */}
+            <Sidebar />
         </>
     )
 }
