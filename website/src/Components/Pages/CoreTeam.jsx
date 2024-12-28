8112191b549483f3b21a4a2a875a5f08ad95edd1
yuvi <yuvidume2@gmail.com>
2024-12-28 12:50:47 +0530

    made a simple core team page

--- website/src/Components/Pages/CoreTeam.jsx
+++ website/src/Components/Pages/CoreTeam.jsx
@@ -1,7 +1,58 @@
+import "./coreTeam.css";
+
+
 const CoreTeam = () => {
     return (
         <>
-            <p>CoreTeam</p>
+            <div className="title fontstyle"><p>《 CORE TEAM 》</p>
+                <hr class="w-1/2 mx-auto my-5 border-siteGreen border-2"></hr></div>
+            <div className="cardcontainer">
+                <div className="portfolios"><h1 className="cardHeading" >Secretory</h1>
+                    <div className="cards"> <div className="teamCard">
+                        <img className="profilePic" src="\images\profilePic.avif" alt="" />
+                        <div className="teamInfo fontstyle"><h1>YUVRAJ</h1>
+                            <div className="teamContacts">
+                                <img src="\images\insta_icon.png" alt="" />
+                                <img src="\images\linkdin_icon.png" alt="" />
+                            </div>
+                        </div>
+                    </div></div>
+                </div>
+                <div className="portfolios"><h1 className="cardHeading" >Overall Co-ordinator</h1>
+                    <div className="cards"> <div className="teamCard">
+                        <img className="profilePic" src="\images\profilePic.avif" alt="" />
+                        <div className="teamInfo fontstyle"><h1>YUVRAJ</h1>
+                            <div className="teamContacts">
+                                <img src="\images\insta_icon.png" alt="" />
+                                <img src="\images\linkdin_icon.png" alt="" />
+                            </div>
+                        </div>
+                    </div></div>
+                </div>
+                <div className="portfolios">
+                    <h1 className="cardHeading" >WEB n APP</h1>
+                    <div className="cards">
+                        <div className="teamCard">
+                            <img className="profilePic" src="\images\profilePic.avif" alt="" />
+                            <div className="teamInfo fontstyle"><h1>YUVRAJ</h1>
+                                <div className="teamContacts">
+                                    <img src="\images\insta_icon.png" alt="" />
+                                    <img src="\images\linkdin_icon.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="teamCard">
+                            <img className="profilePic" src="\images\profilePic.avif" alt="" />
+                            <div className="teamInfo fontstyle"><h1>YUVRAJ</h1>
+                                <div className="teamContacts">
+                                    <img src="\images\insta_icon.png" alt="" />
+                                    <img src="\images\linkdin_icon.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div></div>
+            </div>
+
         </>
     )
 }
