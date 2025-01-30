4fdae3489682676afb893aa01bd8e8e55f7cef79
2052dhanu <dhanshreerode283@gmail.com>
2025-01-30 10:21:55 +0530

    changes

--- website/src/Components/Pages/Gallery.jsx
+++ website/src/Components/Pages/Gallery.jsx
@@ -7,36 +7,17 @@ const Gallery = () => {
     "galleryp/IMG20230213132100.jpg",
     "galleryp/IMG20230211124714.jpg",
     "galleryp/IMG20230211120323_BURST009.jpg",
+    "galleryp/20230211_120848.jpg",
     "galleryp/IMG_20230211_092831.jpg",
     "galleryp/IMG_4595.JPG",
+    
+    "galleryp/IMG_3692.JPG",
     "galleryp/IMG_4412.JPG",
-    "galleryp/20230211_120214.jpg",
+    
+    
+    ""
   ];
 
-  useEffect(() => {
-    const imageElements = document.querySelectorAll('.image-item img');
-    let hasWideImage = false;
-    let hasTallImage = false;
-
-    imageElements.forEach(image => {
-      if (image.naturalWidth > image.naturalHeight) {
-        hasWideImage = true;
-      } else if (image.naturalHeight > image.naturalWidth) {
-        hasTallImage = true;
-      }
-    });
-
-    const galleryElement = document.querySelector('.image-gallery');
-    if (hasWideImage) {
-      galleryElement.classList.add('wide-layout');
-    }
-    if (hasTallImage) {
-      galleryElement.classList.add('tall-layout');
-    }
-    if (!hasWideImage && !hasTallImage) {
-      galleryElement.classList.add('normal-layout');
-    }
-  }, []);
 
   return (
     <div className="container">
