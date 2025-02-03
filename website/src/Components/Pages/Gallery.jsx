c9e5b1ee3d74e345531212fbc7b2868e7b85d72e
Tejas2171 <tejassonawane11102004@gmail.com>
2025-02-03 12:22:58 +0530

    sponsers working

--- website/src/Components/Pages/Gallery.jsx
+++ website/src/Components/Pages/Gallery.jsx
@@ -59,6 +59,7 @@ const Gallery = () => {
 
   useEffect(() => {
     setShuffledImages(shuffleArray(images));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // Runs only once when component mounts
 
   return (
